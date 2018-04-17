2cbe411f9d1636207608b888ed34f19716e64440
rolandus10 <rolandguy18@gmail.com>
2018-04-17 10:57:31 +0200

    update test

--- MongoWireProtocol/src/main/java/com/mycompany/mongowireprotocol/Main.java
+++ MongoWireProtocol/src/main/java/com/mycompany/mongowireprotocol/Main.java
@@ -17,41 +17,36 @@ import java.net.Socket;
  *
  * @author sonoflight
  */
-public class Main {
-private static final int PORT = 9632;
+public final class Main {
+    private static final int PORT = 9632;
+    private static final int PORT_DB = 27017;
+    private Main() { }
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
+    public static void main(final String[] args) {
         try {
             ServerSocket socket =  new ServerSocket(PORT);
             Socket client = socket.accept();
             System.out.println("client connection reussie!");
-           
             BufferedReader in = new BufferedReader(
                         new InputStreamReader(client.getInputStream()));
             PrintStream db = new PrintStream(client.getOutputStream());
             String msg = in.readLine();
             System.out.println("Msg client :" + msg);
-            
-            
-            Socket com = new Socket ("127.0.0.1",27017);
+            Socket com = new Socket("127.0.0.1", PORT_DB);
             System.out.println("connection reussie!");
-            
             PrintWriter out = new PrintWriter(com.getOutputStream());
             out.println(msg);
             System.out.println("msg envoyé");
-            
             BufferedReader in_com = new BufferedReader(
                         new InputStreamReader(com.getInputStream()));
             String db_msg = in_com.readLine();
             db.println(db_msg);
             System.out.println("DB msg :" + db_msg);
             db.println(db_msg);
-            
-           
         } catch (IOException ex) {
             System.out.println(ex.getMessage());
         }
-}
+    }
 }
