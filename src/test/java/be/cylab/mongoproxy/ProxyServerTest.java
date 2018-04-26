f87aa14e2f86e03bd71c0ee6e87f26a5b197b085
Thibault Debatty <thibault.debatty@gmail.com>
2018-04-26 12:28:07 +0200

    Renamed project and packages

--- src/test/java/be/cylab/mongoproxy/ProxyServerTest.java
+++ src/test/java/be/cylab/mongoproxy/ProxyServerTest.java
@@ -3,8 +3,9 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package com.mycompany.mongowireprotocol;
+package be.cylab.mongoproxy;
 
+import be.cylab.mongoproxy.ProxyServer;
 import com.mongodb.MongoClient;
 import com.mongodb.client.MongoCollection;
 import com.mongodb.client.MongoDatabase;
